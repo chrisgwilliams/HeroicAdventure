adea5fa43f44a82d8c52c31691dcab48e0a940a6
Chris G. Williams <chrisgwilliams@gmail.com>
2017-12-09 18:51:23 -0500

    Quest Refactor

--- HA/Common/Structures.vb
+++ HA/Common/Structures.vb
@@ -44,33 +44,4 @@
         Public Achieved As Boolean
     End Structure
 
-    Public Structure Quest
-        Public Name As String
-        Public Questgiver As NPC
-        Public Status As QuestStatus
-        Public ReportTo As NPC  ' Used for MeetingQuest, KillQuest, and FedExQuest
-
-        ' Quest Types
-        Public MeetingQuest As Boolean
-        Public KillQuest As Boolean
-        Public FedExQuest As Boolean
-
-        ' Kill Quest
-        Public KillType As Monster
-        Public KillGoal As Int16
-        Public KillCount As Int16
-
-        ' FedEx Quest
-        Public PackageList As ArrayList
-
-        ' Rewards
-        Public XPReward As Int16
-        Public GPReward As Int16
-        Public ItemReward As ItemBase
-
-        ' Conversation
-        Public IntroMsg() As String
-        Public UnresolvedMsg() As String
-        Public ResolvedMsg() As String
-    End Structure
 End Namespace
---
+++ HA/Quest/Quest.vb
@@ -0,0 +1,41 @@
+﻿Imports HA.Common
+Imports System.Collections.Generic
+
+Public Class Quest
+
+    Public Name As String
+    Public Status As QuestStatus
+
+    Public Source As NPC
+    Public Destination As NPC
+
+    ' Quest Types
+    Public MeetingQuest As Boolean
+    Public KillQuest As Boolean
+    Public FedExQuest As Boolean
+
+    ' Kill Quest
+    Public KillType As Monster
+    Public KillGoal As Int16
+    Public KillCount As Int16
+
+    ' FedEx Quest
+    Public PackageList As List(Of ItemBase)
+
+    ' Rewards
+    Public XPReward As Int16
+    Public GPReward As Int16
+    Public ItemReward As ItemBase
+
+    ' Conversation
+    Public IntroMsg() As String
+    Public UnresolvedMsg() As String
+    Public ResolvedMsg() As String
+
+    Public Sub New()
+        PackageList = New List(Of ItemBase)
+
+
+    End Sub
+
+End Class
